4b20d8543596eb0f609255c94d6cfc20b757ca6e
fruchtblase <fruchtblase@users.noreply.github.com>
2021-07-19 13:03:54 +0200

    fix to make the plugin work again
    implemented solution from this issue

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian'
 export default class MyPlugin extends Plugin {
 	onload() {
 		this.app.workspace.on('file-open', () => {
-			this.app.commands.executeCommandById('explorer:reveal-active-file');
+			this.app.commands.executeCommandById('file-explorer:reveal-active-file');
 		})	
 	}
 }

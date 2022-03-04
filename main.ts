adc52d3ecf53ab977e9bfab0be372fc0d16bfd8b
Yu Wang <yuwan@microsoft.com>
2022-03-03 20:29:15 -0800

    fix: only reveal when file explorer is visible

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 import { Plugin, Command } from 'obsidian';
-
+declare module "obsidian" {
+	interface WorkspaceLeaf {
+		width: Number;
+	}
+}
 export default class MyPlugin extends Plugin {
 	onload() {
 		this.app.workspace.on('file-open', () => {
-			(this.app as any).commands.executeCommandById('file-explorer:reveal-active-file');
-		})	
+			const workspace = this.app.workspace;
+			workspace.iterateAllLeaves((leaf) => {
+				if(leaf.getViewState().type == "file-explorer" && leaf.width > 0)
+				{
+					(this.app as any).commands.executeCommandById('file-explorer:reveal-active-file');
+				}
+			});
+
+		})
 	}
 }

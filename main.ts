cf22ba00fdeaa8c0857805b1d46dbb4fc53815b4
Yu Wang <yuwan@microsoft.com>
2022-03-13 06:53:40 -0700

    fix: only reveal when file explorer is visible
    The current experience is:
    - When file explorer is visible, auto reveal
    - When file explorer is visible, disable auto reveal, so the sidebar will not auto popup. The reasons are two folds: 1) the auto popup creates a distraction while writing. 2) when using in a reduced window size auto popup ceates a hassle to hide sidebar everytime I create or navigate a new page
    - When file explorer is changed from hidden to visible, auto reveal. So the experience is still very similar to original plugin.

--- main.ts
+++ main.ts
@@ -1,20 +1,47 @@
-import { Plugin, Command } from 'obsidian';
+import { Plugin, Command, Workspace } from 'obsidian';
 declare module "obsidian" {
 	interface WorkspaceLeaf {
 		width: Number;
 	}
 }
 export default class MyPlugin extends Plugin {
-	onload() {
-		this.app.workspace.on('file-open', () => {
-			const workspace = this.app.workspace;
-			workspace.iterateAllLeaves((leaf) => {
-				if(leaf.getViewState().type == "file-explorer" && leaf.width > 0)
-				{
-					(this.app as any).commands.executeCommandById('file-explorer:reveal-active-file');
-				}
-			});
+	private is_file_explorer_open_previously = false;
 
+	private is_file_explorer_open()
+	{
+		const workspace = this.app.workspace;
+		let is_open = false;
+		workspace.iterateAllLeaves((leaf) => {
+			if(leaf.getViewState().type == "file-explorer" && leaf.width > 0)
+			{
+				is_open = true;
+			}
+		});
+		return is_open;
+
+	}
+	private reveal()
+	{
+		(this.app as any).commands.executeCommandById('file-explorer:reveal-active-file');
+	}
+
+	onload() {
+		this.is_file_explorer_open_previously = this.is_file_explorer_open();
+
+		this.app.workspace.on('file-open', () => {
+			if(this.is_file_explorer_open())
+			{
+				this.reveal();
+			}
+		})
+
+		this.app.workspace.on('click', () => {
+			const is_file_explorer_open_now = this.is_file_explorer_open();
+			if(is_file_explorer_open_now && ! this.is_file_explorer_open_previously)
+			{
+				this.reveal();
+			}
+			this.is_file_explorer_open_previously = is_file_explorer_open_now;
 		})
 	}
 }

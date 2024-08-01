347dec74b66537f3d77496cdad30b602a8b2f45e
yongjae <lnyarl@gmail.com>
2024-08-01 14:53:33 +0900

    fix: remove unsupported event in obsidian api

--- main.ts
+++ main.ts
@@ -35,7 +35,7 @@ export default class MyPlugin extends Plugin {
 			}
 		})
 
-		this.app.workspace.on('click', async () => {
+		this.app.workspace.on('active-leaf-change', async (leaf: WorkspaceLeaf) => {
 			await new Promise(resolve => setTimeout(resolve, 200));
 			const is_file_explorer_open_now = this.is_file_explorer_open();
 			console.log(`is_file_explorer_open_previously: ${this.is_file_explorer_open_previously}, is_file_explorer_open_now: ${is_file_explorer_open_now}`);
